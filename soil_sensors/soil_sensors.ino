3e0c800fb6d6ec5104c091e5a2824a15c5d87e9a
Tomoooji <168693891+Tomoooji@users.noreply.github.com>
2026-07-07 12:12:11 +0900

    create .cpp file

---
+++ soil_sensors/soil_sensors.ino
@@ -0,0 +1,17 @@
+/* Soil sensors
+Arduino UNO
+  サーミスタ
+  TDSセンサ
+  土壌水分センサー
+  + microSD?
+*/
+
+constexpr uint8_t pin_thrm = A0; //サーミスタ
+constexpr uint8_t pin_tds = A1; //TDSセンサ
+constexpr uint8_t pin_mist = A2; //土壌水分(湿度)センサ
+
+constexpr int B_thrm = 3435; //K
+
+void setup(){}
+
+void loop(){}
