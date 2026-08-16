7ca6134722c3ba80806c4e25e4366d9be19c403c
Tomoooji <168693891+Tomoooji@users.noreply.github.com>
2026-08-16 15:45:13 +0900

    ,

--- soil_sensors/soil_sensors.ino
+++ soil_sensors/soil_sensors.ino
@@ -12,6 +12,27 @@ constexpr uint8_t pin_mist = A2; //土壌水分(湿度)センサ
 
 constexpr int B_thrm = 3435; //K
 
-void setup(){}
 
-void loop(){}
+
+#define SAMPLE_NUM 16
+
+const int pin = 15;
+
+int buffer[SAMPLE_NUM];
+unsigned int head = 0;
+
+void setup() {
+  pinMode(pin,INPUT);
+  Serial.begin(115200);
+}
+
+void loop() {
+  buffer[head & (SAMPLE_NUM-1)] = analogRead(pin);
+  head+=1;
+  long average = 0;
+  for (int val : buffer){
+    average += val;
+  }
+  average = average / SAMPLE_NUM;
+  Serial.println(average);
+}
